b42f1ed86ee18ace04092939d4dcb1ed512a35bf
akbar cp <akbarcp607@gmail.com>
2023-04-16 06:53:37 +0530

    localisation_added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from jinja2 import Template
+from keras.models import load_model
+import base64
 from fastapi import FastAPI
 from pydantic import BaseModel
 import tensorflow as tf
@@ -26,63 +29,59 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
-
-
-
-
 # %%
 
 
-
 # %%
-def convert_to_ela_image(path,quality):
+def convert_to_ela_image(path, quality):
     temp_filename = 'temp_file_name.jpg'
     ela_filename = 'temp_ela.png'
-    
-    image =  PIL.Image.open(path).convert('RGB')
-    image.save(temp_filename,'JPEG',quality = quality)
+
+    image = PIL.Image.open(path).convert('RGB')
+    image.save(temp_filename, 'JPEG', quality=quality)
     temp_image = PIL.Image.open(temp_filename)
-    
-    ela_image = ImageChops.difference(image,temp_image)
-    
+
+    ela_image = ImageChops.difference(image, temp_image)
+
     extrema = ela_image.getextrema()
     max_diff = max([ex[1] for ex in extrema])
     if max_diff == 0:
-        max_diff=1
+        max_diff = 1
     scale = 255.0 / max_diff
     ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
     return ela_image
 
 # %%
 
+
 def build_model():
 
- model = Sequential()
- model.add(Conv2D(32, kernel_size = (5,5), input_shape = (128,128,3), activation ='relu', padding ="same"))
- model.add(MaxPool2D(pool_size = (2,2)))
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(5, 5), input_shape=(
+        128, 128, 3), activation='relu', padding="same"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
 
- model.add(Conv2D(32, kernel_size = (5,5), activation='relu', padding ='same')) 
- model.add(MaxPool2D(pool_size =(2,2), padding ='same'))
+    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', padding='same'))
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
 
- model.add(Conv2D(64, kernel_size = (5,5), activation='relu', padding ='same')) 
- model.add(MaxPool2D(pool_size =(2,2), padding ='same'))
+    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'))
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
 
- model.add(Conv2D(64, kernel_size = (5,5), activation='relu', padding ='same')) 
- model.add(MaxPool2D(pool_size =(2,2), padding ='same'))
+    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu', padding='same'))
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
 
- model.add(Flatten())
+    model.add(Flatten())
 
- model.add(Dense(64, activation = "relu"))
- model.add(Dense(32, activation = "relu"))
- 
- model.add(Dense(2, activation="softmax"))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(32, activation="relu"))
 
+    model.add(Dense(2, activation="softmax"))
 
+    return model
 
- return model;
 
 # %%
-model=build_model()
+model = build_model()
 model.summary()
 
 # %%
@@ -96,68 +95,76 @@ batch_size = 22
 
 
 # %%
-from keras.models import load_model
 model = load_model('model_casia_run1.h5')
 
 
 # %%
-image_size=(128,128)
+image_size = (128, 128)
 
 # %%
+
+
 def prepare_image(image_path):
-    return np.array(convert_to_ela_image(image_path,90).resize(image_size)).flatten() / 255.0
+    return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0
+
 
 # %%
-class_names = ['forged','original']
+class_names = ['forged', 'original']
 
 # %%
 real_image_path = 'basedata/training/original/Tr-me_0082.jpg'
 
-image =prepare_image (real_image_path)
+image = prepare_image(real_image_path)
 
-image =image.reshape(-1, 128, 128, 3)
+image = image.reshape(-1, 128, 128, 3)
 
-y_pred= model.predict(image)
+y_pred = model.predict(image)
 
-y_pred_class =np.argmax(y_pred, axis= 1)[0]
+y_pred_class = np.argmax(y_pred, axis=1)[0]
 
-print(f'Class: {class_names[y_pred_class]} Confidence: {np.amax(y_pred) *100:0.2f}')
+print(
+    f'Class: {class_names[y_pred_class]} Confidence: {np.amax(y_pred) *100:0.2f}')
 
 # %%6
 real_image_path = 'basedata/validation/original/Tr-me_0010.jpg'
 
-image =prepare_image (real_image_path)
+image = prepare_image(real_image_path)
 
-image =image.reshape(-1, 128, 128, 3)
+image = image.reshape(-1, 128, 128, 3)
 
-y_pred= model.predict(image)
+y_pred = model.predict(image)
 print(y_pred)
 
-y_pred_class =np.argmax(y_pred, axis= 1)[0]
+y_pred_class = np.argmax(y_pred, axis=1)[0]
 
-confidence= np.amax(y_pred) *100
+confidence = np.amax(y_pred) * 100
 
-print(f'Class: {class_names[y_pred_class]} Confidence: {np.amax(y_pred) *100:0.2f}')
+print(
+    f'Class: {class_names[y_pred_class]} Confidence: {np.amax(y_pred) *100:0.2f}')
+
 
 @app.get("/")
 async def root():
-	return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
 
 class Item(BaseModel):
     name: str
     price: float
 
-@app.get ("/home")
+
+@app.get("/home")
 async def read_index():
-    return FileResponse("index.html")    
- 
+    return FileResponse("index.html")
+
 
 @app.post("/items/")
 async def create_item(item: Item):
     """
     Create an item with the provided name and price.
     """
-    return {"item": item , "Confidence": confidence}
+    return {"item": item, "Confidence": confidence}
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...),):
@@ -169,38 +176,26 @@ async def create_upload_file(file: UploadFile = File(...),):
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
     app.mount("/", StaticFiles(directory="/"), name="images")
-    imagea =prepare_image (file_path)
-    
-    image1 =imagea.reshape(-1, 128, 128, 3)
+    imagea = prepare_image(file_path)
 
-    y_pred1= model.predict(image1)
-  
+    image1 = imagea.reshape(-1, 128, 128, 3)
 
-    y_pred_class1 = np.argmax(y_pred1, axis= 1)[0]
+    y_pred1 = model.predict(image1)
+    y_pred_class2 = class_names[y_pred_class]
+    y_pred_class1 = np.argmax(y_pred1, axis=1)[0]
+    base_path = os.getcwd()
+    print(base_path)
+    confidence = np.amax(y_pred) * 100
+    forged = 100 - confidence
+    localised_path = f"{base_path}/{file_path}"
+    localised= convert_to_ela_image(localised_path,90)
 
-    confidence= np.amax(y_pred) *100
-    print(file_path)
-    image1_path = "/images/{file_path}"
-    # image2_path = "/images/image2.jpg"
-    html_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Upload Successful!</title>
-    </head>
-    <body style="font-family: Arial, sans-serif; background-color: #f1f1f1; text-align: center;">
-        <h1 style="color: #4CAF50; margin-top: 50px;">Upload Successful!</h1>
-        <p style="font-size: 20px; color: #555; margin-bottom: 25px;">Confidence Percentage: {confidence}</p>
-        <div style="display: flex; justify-content: center; align-items: center; margin-top: 50px;">
-            <img style="margin: 10px; width: 300px; height: auto; border: 1px solid #ccc; border-radius: 5px;" src="" alt="Image 1" title="Title for Image 1">
-            <img style="margin: 10px; width: 300px; height: auto; border: 1px solid #ccc; border-radius: 5px;" src="" alt="Image 2" title="Title for Image 2">
-        </div>
-        <div style="font-size: 18px; font-weight: bold; color: #888; margin-top: 10px;">Title for both images</div>
-    </body>
-    </html>
-    """.format(confidence=confidence)
-   
- 
-
+    if os.path.exists(file_path):
+        print("File exists!")
+    else:
+        print("File does not exist!")
+    template = Template(open("./uploded.html").read())
+    rendered_template = template.render(
+        forged=forged, file_name=f'{base_path}/{file_path}' ,localised=f'{base_path}/temp_file_name.jpg' , classmame=y_pred_class2)
     # Return the file path and caption
-    return HTMLResponse(content=html_content)
+    return HTMLResponse(rendered_template, media_type="text/html")
